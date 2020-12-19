16c9ce3ad2f26021fd03063680369a5fcfc0827d
Dylan Le <led7950@my.sduhsd.net>
2020-12-19 02:14:13 +0000

    final version of project

--- dlstatus.sh
+++ dlstatus.sh
@@ -1,12 +1,20 @@
-#!/bin/bash                                                                                                       
-# fileinfo                                                                                                        
-
+#!/bin/bash
+# fileinfo
 DROPLETS="dl01 dl02 dl03 dl04 dl05 dl06 dl07 dl08 dl09 dl10 dl11"
 
 
 for droplet in $DROPLETS ; do
 
-   ping $droplet -c 1 
+ping $droplet -c 1 -W 2 > /dev/null
+
+ if [ $? = 0 ]; then
+	echo "$droplet : alive"
+	continue
+ else
+	echo "$droplet : dead"
+	continue
+ fi
+
 done
 
 exit 0
